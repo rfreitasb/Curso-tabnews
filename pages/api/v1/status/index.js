8be8189fe8b7745e4422bc013061709a86f67cf1
Ramon Batista <rfreitasbat@gmail.com>
2026-05-08 23:21:33 +0000

    Criando arquivo compose.yaml, usando variaveis de ambiente e database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,12 @@
-function status(request, response) {
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;")
+  console.log(result)
   response.status(200).json({
     status: 200,
     mensagem: "teste de status 200 e configuração de formatação de texto",
+    resultadoQuery: result
   });
 }
 
